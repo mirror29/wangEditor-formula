e894ad18d02f9fb28a0afd93f68e56b1c9bef528
clinfc <1962109568@qq.com>
2020-08-05 10:43:33 +0800

    feat: wangEditor demo

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # wangEdior-with-vue
 wangEditor 在 vue 中的使用
+
+
+### 开始使用
+
+```
+# 克隆项目
+git clone git@github.com:wangeditor-team/wangEdior-with-vue.git
+
+# 进入项目目录
+cd wangEdior-with-vue
+
+# 安装依赖
+npm install
+
+# 运行项目（将自动在浏览器打开 http://localhost:8080 ）
+npm run serve
+```
---
+++ src/views/Home.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="home">
+    <h3>wangEditor with vue</h3>
+    <div id="demo1"></div>
+    <button type="button" class="btn" @click="getEditorData">获取当前内容</button>
+    <h3>内容预览</h3>
+    <textarea name="" id="" cols="170" rows="20" readonly v-model="editorData"></textarea>
+  </div>
+</template>
+
+<script>
+
+// 引入 wangEditor
+import wangEditor from '@wangeditor-team/we-next'
+
+export default {
+  data() {
+    return {
+      editor: null,
+      editorData: ''
+    }
+  },
+  mounted() {
+    const editor = new wangEditor(`#demo1`)
+
+    // 配置 onchange 回调函数，将数据同步到 vue 中
+    editor.config.onchange = (newHtml) => {
+       this.editorData = newHtml
+    }
+
+    // 创建编辑器
+    editor.create()
+
+    this.editor = editor
+  },
+  methods: {
+    getEditorData() {
+      // 通过代码获取编辑器内容
+      let data = this.editor.txt.html()
+      alert(data)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .home {
+    width: 1200px;
+    margin: auto;
+    position: relative;
+    .btn {
+      position: absolute;
+      right: 0;
+      top: 0;
+      padding: 5px 10px;
+      cursor: pointer;
+    }
+    h3 {
+      margin: 30px 0 15px;
+    }
+  }
+</style>
