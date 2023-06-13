089df79b4fb71f291ab078a2e62dee277ba67f9f
陈颖 <chenying@qtrade.com.cn>
2023-06-13 18:02:20 +0800

    feat: 公式模块初始化

--- src/components/BasicEditor.vue
+++ src/components/BasicEditor.vue
@@ -16,7 +16,7 @@
         :defaultConfig="editorConfig"
         :mode="mode"
         v-model="valueHtml"
-        style="height: 400px; overflow-y: hidden;"
+        style="height: 650px; overflow-y: hidden;"
         @onCreated="handleCreated"
         @onChange="handleChange"
         @onDestroyed="handleDestroyed"
@@ -36,126 +36,106 @@
   </div>
 </template>
 
-<script>
+<script setup>
 import "@wangeditor/editor/dist/css/style.css";
 import { onBeforeUnmount, ref, shallowRef, onMounted } from "vue";
 import { Editor, Toolbar } from "@wangeditor/editor-for-vue";
-import menu from "./Editor/kityformula.js";
+import kityformula from "./Editor/kityformula.js";
+import myscriptMath from "./Editor/myscriptMath";
 import { Boot } from "@wangeditor/editor";
 import formulaModule from "@wangeditor/plugin-formula";
 
+// 插件注入
 Boot.registerModule(formulaModule);
+Boot.registerMenu(kityformula);
+Boot.registerMenu(myscriptMath);
 
-export default {
-  components: { Editor, Toolbar },
-  setup() {
-    // 编辑器实例，必须用 shallowRef，重要！
-    const editorRef = shallowRef();
+// 编辑器实例，必须用 shallowRef，重要！
+const editorRef = shallowRef();
 
-    // 内容 HTML
-    const valueHtml = ref("<p>hello</p>");
+// 内容 HTML
+const valueHtml = ref("<p>hello</p>");
 
-    Boot.registerMenu(menu);
+// 模拟 ajax 异步获取内容
+onMounted(() => {
+  setTimeout(() => {
+    valueHtml.value = "<p>模拟 Ajax 异步设置内容</p>";
+  }, 1500);
+});
 
-    // 模拟 ajax 异步获取内容
-    onMounted(() => {
-      setTimeout(() => {
-        valueHtml.value = "<p>模拟 Ajax 异步设置内容</p>";
-      }, 1500);
-    });
-
-    const toolbarConfig = {
-      insertKeys: {
-        index: 0,
-        keys: [
-          "insertFormula", // “插入公式”菜单
-          // 'editFormula' // “编辑公式”菜单
-        ],
-      },
-    };
-    const editorConfig = {
-      placeholder: "请输入内容...",
-      hoverbarKeys: {
-        formula: {
-          menuKeys: ["editFormula"], // “编辑公式”菜单
-        },
-      },
-    };
-
-    // 组件销毁时，也及时销毁编辑器，重要！
-    onBeforeUnmount(() => {
-      const editor = editorRef.value;
-      if (editor == null) return;
-
-      editor.destroy();
-    });
-
-    // 编辑器回调函数
-    const handleCreated = (editor) => {
-      console.log("created", editor);
-      editorRef.value = editor; // 记录 editor 实例，重要！
-    };
-    const handleChange = (editor) => {
-      console.log("change:", editor.getHtml());
-    };
-    const handleDestroyed = (editor) => {
-      console.log("destroyed", editor);
-    };
-    const handleFocus = (editor) => {
-      console.log("focus", editor);
-    };
-    const handleBlur = (editor) => {
-      console.log("blur", editor);
-    };
-    const customAlert = (info, type) => {
-      alert(`【自定义提示】${type} - ${info}`);
-    };
-    const customPaste = (editor, event, callback) => {
-      console.log("ClipboardEvent 粘贴事件对象", event);
-
-      // 自定义插入内容
-      editor.insertText("xxx");
-
-      // 返回值（注意，vue 事件的返回值，不能用 return）
-      callback(false); // 返回 false ，阻止默认粘贴行为
-      // callback(true) // 返回 true ，继续默认的粘贴行为
-    };
-
-    const insertText = () => {
-      const editor = editorRef.value;
-      if (editor == null) return;
-      editor.insertText("hello world");
-    };
-
-    const printHtml = () => {
-      const editor = editorRef.value;
-      if (editor == null) return;
-      console.log(editor.getHtml());
-    };
-
-    const disable = () => {
-      const editor = editorRef.value;
-      if (editor == null) return;
-      editor.disable();
-    };
-
-    return {
-      editorRef,
-      mode: "default",
-      valueHtml,
-      toolbarConfig,
-      editorConfig,
-      handleCreated,
-      handleChange,
-      handleDestroyed,
-      handleFocus,
-      handleBlur,
-      customAlert,
-      customPaste,
-      insertText,
-      printHtml,
-      disable,
-    };
+const toolbarConfig = {
+  insertKeys: {
+    index: 0,
+    keys: [
+      "insertFormula", // “插入公式”菜单
+      "kityFormula", // “编辑公式”菜单
+      "myscriptMath", //"手写公式"菜单
+    ],
   },
+};
+const editorConfig = {
+  placeholder: "请输入内容...",
+  hoverbarKeys: {
+    formula: {
+      // menuKeys: ["kityFormula"], // “编辑公式”菜单
+    },
+  },
+};
+
+// 组件销毁时，也及时销毁编辑器，重要！
+onBeforeUnmount(() => {
+  const editor = editorRef.value;
+  if (editor == null) return;
+
+  editor.destroy();
+});
+
+// 编辑器回调函数
+const handleCreated = (editor) => {
+  console.log("created", editor);
+  editorRef.value = editor; // 记录 editor 实例，重要！
+};
+const handleChange = (editor) => {
+  console.log("change:", editor.getHtml());
+};
+const handleDestroyed = (editor) => {
+  console.log("destroyed", editor);
+};
+const handleFocus = (editor) => {
+  console.log("focus", editor);
+};
+const handleBlur = (editor) => {
+  console.log("blur", editor);
+};
+const customAlert = (info, type) => {
+  alert(`【自定义提示】${type} - ${info}`);
+};
+const customPaste = (editor, event, callback) => {
+  console.log("ClipboardEvent 粘贴事件对象", event);
+
+  // 自定义插入内容
+  editor.insertText("xxx");
+
+  // 返回值（注意，vue 事件的返回值，不能用 return）
+  callback(false); // 返回 false ，阻止默认粘贴行为
+  // callback(true) // 返回 true ，继续默认的粘贴行为
+};
+
+const insertText = () => {
+  const editor = editorRef.value;
+  if (editor == null) return;
+  editor.insertText("hello world");
+};
+
+const printHtml = () => {
+  const editor = editorRef.value;
+  if (editor == null) return;
+  console.log(editor.getHtml());
+};
+
+const disable = () => {
+  const editor = editorRef.value;
+  if (editor == null) return;
+  editor.disable();
 };
 </script>
--- src/components/Editor/kityformula.js
+++ src/components/Editor/kityformula.js
@@ -1,25 +1,97 @@
-class MyButtonMenu {
+import $ from "jquery";
+import { formulaIcon } from "../../assets/icon/svg-icon.ts";
+
+class MyKityFormulaMenu {
   constructor() {
-    (this.title = "menu12222"), (this.tag = "button");
+    this.title = "编辑公式";
+    this.iconSvg = formulaIcon;
+    this.tag = "button";
+    this.showModal = true;
+    this.modalWidth = 900;
+    this.modalHeight = 600;
   }
-  getValue() {
+
+  // 菜单是否需要激活（如选中加粗文本，“加粗”菜单会激活），用不到则返回 false
+  isActive(editor) {
+    return false;
+  }
+
+  // 获取菜单执行时的 value ，用不到则返回空 字符串或 false
+  getValue(editor) {
     return "";
   }
-  isActive() {
+
+  // 菜单是否需要禁用（如选中 H1 ，“引用”菜单被禁用），用不到则返回 false
+  isDisabled(editor) {
     return false;
   }
-  isDisabled() {
-    return false;
+  // 点击菜单时触发的函数
+  exec(editor, value) {
+    // Modal menu ，这个函数不用写，空着即可
   }
-  exec(editor) {
-    console.log(editor);
-    alert("menu1 exec");
+
+  // 弹出框 modal 的定位：1. 返回某一个 SlateNode； 2. 返回 null （根据当前选区自动定位）
+  getModalPositionNode(editor) {
+    return null; // modal 依据选区定位
+  }
+
+  // 定义 modal 内部的 DOM Element
+  getModalContentElem(editor) {
+    // panel 中需要用到的id
+    const inputIFrameId = "kityformula_" + Math.ceil(Math.random() * 10);
+    const btnOkId = "kityformula-btn" + Math.ceil(Math.random() * 10);
+
+    const $content = $(`
+    <div>
+      <iframe id="${inputIFrameId}" class="iframe" height="500px" width="100%" frameborder="0" scrolling="no" src="/kityformula/index.html"></iframe>
+    </div>`);
+    const $button = $(
+      `<button id="${btnOkId}" class="right" style='margin: 5px 0'>
+        确认插入
+      </button>`
+    );
+    $content.append($button);
+
+    $button.on("click", () => {
+      // 执行插入公式
+      const node = document.getElementById(inputIFrameId);
+      const kfe = node.contentWindow.kfe;
+
+      kfe.execCommand("get.image.data", function(data) {
+        // 获取base64
+        // console.log(data.img);
+      });
+
+      let latex = kfe.execCommand("get.source");
+      latex = latex.replace(/\s/g, ""); // 去掉空格
+
+      const formulaNode = {
+        type: "paragraph",
+        children: [
+          {
+            type: "formula",
+            value: latex,
+            children: [
+              {
+                text: "",
+              },
+            ],
+          },
+        ],
+      };
+      editor.insertNode(formulaNode);
+      editor.hidePanelOrModal();
+    });
+
+    return $content[0]; // 返回 DOM Element 类型
+
+    // PS：也可以把 $content 缓存下来，这样不用每次重复创建、重复绑定事件，优化性能
   }
 }
 const menuConf = {
-  key: "my-menu-1", // menu key ，唯一。注册之后，需通过 toolbarKeys 配置到工具栏
+  key: "kityFormula", // menu key ，唯一。注册之后，需通过 toolbarKeys 配置到工具栏
   factory() {
-    return new MyButtonMenu();
+    return new MyKityFormulaMenu();
   },
 };
 
